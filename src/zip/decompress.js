7e6e1e71f33c461d08c0a62cd0764ce8503a792e
zzVIPz <43119686+zzVIPz@users.noreply.github.com>
2024-01-29 02:44:32 -0500

    feat: task zip decompress is done

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+import { join } from 'path';
+import { throwError } from '../utils/utils.js';
+
 const decompress = async () => {
-    // Write your code here 
+  const sourceFile = join(import.meta.dirname, 'files', 'archive.gz');
+  const outputFile = join(import.meta.dirname, 'files', 'fileToCompress.txt');
+  const readStream = createReadStream(sourceFile);
+  const writeStream = createWriteStream(outputFile);
+  const gunzipStream = createGunzip();
+
+  readStream.pipe(gunzipStream).pipe(writeStream);
+
+  writeStream.on('finish', () => {
+    console.log('Decompressing is done!');
+  });
+
+  writeStream.on('error', (e) => {
+    throwError();
+  });
 };
 
 await decompress();
