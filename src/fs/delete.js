a7fefba0fe632fa5f90d08bc06443a5186301984
zzVIPz <43119686+zzVIPz@users.noreply.github.com>
2024-01-28 23:28:45 -0500

    feat: add delete task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import { unlink } from 'fs/promises';
+import { join } from 'path';
+import { throwError } from '../utils/utils.js';
+
 const remove = async () => {
-    // Write your code here 
+  const sourceFile = join(import.meta.dirname, 'files', 'fileToRemove.txt');
+
+  try {
+    await unlink(sourceFile);
+    console.log(`File fileToRemove was successfully deleted!`);
+  } catch (e) {
+    console.error(`File fileToRemove does not exist!`);
+    throwError();
+  }
 };
 
 await remove();
