bb55d6c4bca4abf23c745077f9b313a74c36adf5
zzVIPz <43119686+zzVIPz@users.noreply.github.com>
2024-01-29 02:01:02 -0500

    feat: add streams read task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { join } from 'path';
+import { throwError } from '../utils/utils.js';
+
 const read = async () => {
-    // Write your code here 
+  const sourceFile = join(import.meta.dirname, 'files', 'fileToRead.txt');
+  const readStream = createReadStream(sourceFile);
+
+  readStream.on('data', (data) => {
+    process.stdout.write(data);
+  });
+
+  readStream.on('error', (e) => {
+    throwError();
+  });
 };
 
 await read();
