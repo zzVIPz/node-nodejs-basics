452c12fb2d463746f102bce40f11fc072cdd3f29
zzVIPz <43119686+zzVIPz@users.noreply.github.com>
2024-01-29 01:48:29 -0500

    feat: add hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,32 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { join } from 'path';
+import { throwError } from '../utils/utils.js';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const sourceFile = join(
+    import.meta.dirname,
+    'files',
+    'fileToCalculateHashFor.txt'
+  );
+  const hash = createHash('sha256');
+  const stream = createReadStream(sourceFile);
+
+  return new Promise((resolve, reject) => {
+    stream.on('data', (data) => {
+      hash.update(data);
+    });
+
+    stream.on('end', () => {
+      const hexHash = hash.digest('hex');
+      console.log(hexHash);
+      resolve(hexHash);
+    });
+
+    stream.on('error', (e) => {
+      reject(throwError());
+    });
+  });
 };
 
 await calculateHash();
