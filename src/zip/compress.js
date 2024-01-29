e8f46a3dcdac654f0e233d5ffa60eb81b818adf5
zzVIPz <43119686+zzVIPz@users.noreply.github.com>
2024-01-29 02:39:44 -0500

    feat: task zip compress is done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import { join } from 'path';
+import { throwError } from '../utils/utils.js';
+
 const compress = async () => {
-    // Write your code here 
+  const sourceFile = join(import.meta.dirname, 'files', 'fileToCompress.txt');
+  const outputFile = join(import.meta.dirname, 'files', 'archive.gz');
+  const readStream = createReadStream(sourceFile);
+  const writeStream = createWriteStream(outputFile);
+  const gzipStream = createGzip();
+
+  readStream.pipe(gzipStream).pipe(writeStream);
+
+  writeStream.on('finish', () => {
+    console.log('Compressing is done!');
+  });
+
+  writeStream.on('error', (e) => {
+    throwError();
+  });
 };
 
 await compress();
