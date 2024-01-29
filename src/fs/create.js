b4df971af5e2dc22a090714de1a3a38540589268
zzVIPz <43119686+zzVIPz@users.noreply.github.com>
2024-01-28 22:59:16 -0500

    feat: add create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { writeFile } from 'fs/promises';
+import { join } from 'path';
+import { isSourceExist, throwError } from '../utils/utils.js';
+
 const create = async () => {
-    // Write your code here 
+  const sourceFile = join(import.meta.dirname, 'files', 'fresh.txt');
+
+  if (await isSourceExist(sourceFile)) {
+    console.error(
+      `FS operation failed: File already exists. Please delete it from 'files' folder and try again`
+    );
+    throwError();
+  } else {
+    await writeFile(sourceFile, 'I am fresh and young');
+    console.log(`File was successfully created!`);
+  }
 };
 
 await create();
