41801125d0df937dbb42aca6c722b00d24a077e1
zzVIPz <43119686+zzVIPz@users.noreply.github.com>
2024-01-29 02:23:48 -0500

    feat: add streams transform task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  const reverseTransform = new Transform({
+    transform(data, _, callback) {
+      callback(null, data.toString().split('').reverse().join('') + '\n');
+    },
+  });
+
+  process.stdin.pipe(reverseTransform).pipe(process.stdout);
 };
 
 await transform();
